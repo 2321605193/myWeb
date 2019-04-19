67b7fec09e6354052301e7f180d00c3808f6dae8
2321605193 <2321605193@qq.com>
2019-04-19 15:02:49 +0800

    Update default.js

--- config/default.js
+++ config/default.js
@@ -1,11 +1,11 @@
 module.exports = {
-  port: 80,
+  port: 3000,
   session: {
     secret: 'myWeb',
     key: 'myWeb',
     maxAge: 2592000000
   },
-  mongodb: 'mongodb://junjun:junjun@127.0.0.1:27017/myWeb'
+  mongodb: 'mongodb://127.0.0.1:27017/myWeb'
 };
 
 
